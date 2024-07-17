37e36ec948ffcda09795dcc7e889b1dba3d9fd51
whitenoise13 <amnesiamar13@gmail.com>
2024-07-17 15:55:59 +0900

    feat :: UnidyServerApplicationTests 설정

---
+++ src/test/java/org/example/unidy_server/UnidyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unidy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
